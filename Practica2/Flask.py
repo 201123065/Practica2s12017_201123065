ab5a6edf3f3128115e9bb4d88352581ddbaee211
marcos mayen <marcosdmayen@gmail.com>
2017-03-05 10:14:15 -0600

    inicio de la cola

--- Practica2/Flask.py
+++ Practica2/Flask.py
@@ -6,21 +6,46 @@ from Lista import Lista
 app = Flask(__name__)
 
 lista= Lista()
+pila = Pila()
 
 class WS():
+	# METODOS DE LA LISTA
+	# INSERTAR
 	@app.route('/insertar_lista',methods=['POST'])
 	def insertar_lista():
 		lista.insertar(request.form['nombre'])
 		return "se ha ingresado correctamente"
-
-
-	@app.route('/consultar',methods=['POST'])
+	# CONSULTAR
+	@app.route('/consultar_en_lista',methods=['POST'])
 	def consultar_lista():
 		valor= lista.consultar(request.form['nombre'])
 		if valor==-1:
 			return "no existe"
 		else:
 			return "se encuentra en el indice "+str(valor)
+	# BORRAR
+	@app.route('/borrar_en_lista',methods=['POST'])
+	def borrar_lista():
+		valor= lista.consultar(request.form['nombre'])
+		if valor==-1:
+			return "no existe"
+		else:
+			return "se encuentra en el indice "+str(valor)
+	# METODOS DE LA PILA 
+	# PUSH
+	@app.route('/push',methods=['POST'])
+	def push():
+		pila.push(request.form['nombre'])
+		return "se ha ingresado correctamente"
+	# POP
+	@app.route('/pop',methods=['POST'])
+	def pop():
+		valor= lista.consultar(request.form['nombre'])
+		if valor==-1:
+			return "no existe"
+		else:
+			return "se encuentra en el indice "+str(valor)
+
 
 	@app.route('/recuperar_lista')
 	def recuperar_lista():
---
+++ Practica2/Pila.py
@@ -0,0 +1,43 @@
+class NodoPila():
+	def __init__(self):
+		self.valor=0
+		self.siguiente=None
+
+class Pila():
+	def __init__(self):
+		self.tope=None
+		self.base=None
+
+	def push(self,valor):
+		Npila = NodoPila()
+		Npila.valor=valor
+		if self.tope==None:
+			self.tope=Npila
+			self.base=self.tope
+		else:
+			self.tope.siguiente=Npila
+			self.tope=self.tope.siguiente
+
+	def pop(self):
+		if self.base==None:
+			return "no existe "
+		elif self.base.siguiente==None:
+			tmp = self.base.valor
+			self.base=None
+			self.tope=None
+			return str(tmp)
+		else:
+			tmp = self.base
+			while tmp.siguiente!=self.tope:
+				tmp=tmp.siguiente
+			temporal = str(self.tope.valor)
+			tmp.siguiente=None
+			self.base=tmp
+			return temporal
+
+
+
+			
+
+
+
